9205a299b9fb7a2df5bca1712bec70b1cb4bcd80
MR3Y <abdonasr379@gmail.com>
2023-05-18 19:04:06 +0300

    Update DefaultNewsRepositoryTest

---
+++ app/src/test/java/com/mr3y/ludi/core/network/fixtures/CacheResponsesDataSource.kt
@@ -0,0 +1,75 @@
+package com.mr3y.ludi.core.network.fixtures
+
+import com.mr3y.ludi.core.model.Result
+import com.mr3y.ludi.core.network.datasources.RSSNewReleasesFeedDataSource
+import com.mr3y.ludi.core.network.datasources.RSSNewsFeedDataSource
+import com.mr3y.ludi.core.network.datasources.RSSReviewsFeedDataSource
+import com.mr3y.ludi.core.network.model.RSSFeedArticle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+open class CacheResponsesDataSource<T: RSSFeedArticle> : RSSNewsFeedDataSource<T>,
+    RSSNewReleasesFeedDataSource<T>, RSSReviewsFeedDataSource<T> {
+
+    val cache: Map<Feed, List<T>?>
+        get() = _cache
+
+    private val _cache: HashMap<Feed, List<T>?> = hashMapOf(Feed.News to null, Feed.Reviews to null, Feed.NewReleases to null)
+    private val simulateFailure = MutableStateFlow(false)
+    private var failureCause: Exception? = null
+
+    protected open fun newsFeed(): List<T> = throw UnsupportedOperationException("$this isn't a RSS News feed provider")
+
+    protected open fun reviewsFeed(): List<T> = throw UnsupportedOperationException("$this isn't a RSS Reviews feed provider")
+
+    protected open fun newReleasesFeed(): List<T> = throw UnsupportedOperationException("$this isn't a RSS New releases feed provider")
+
+    final override suspend fun fetchNewsFeed(): Result<List<T>, Throwable> {
+        return if (_cache[Feed.News] != null) {
+            Result.Success(_cache[Feed.News] as List<T>)
+        } else {
+            delay(300) // simulate network delay
+            return if (simulateFailure.value) Result.Error(failureCause) else Result.Success(newsFeed()).also { _cache[Feed.News] = it.data }
+        }
+    }
+
+    final override suspend fun fetchReviewsFeed(): Result<List<T>, Throwable> {
+        return if (_cache[Feed.Reviews] != null) {
+            Result.Success(_cache[Feed.Reviews] as List<T>)
+        } else {
+            delay(300) // simulate network delay
+            return if (simulateFailure.value) Result.Error(failureCause) else Result.Success(reviewsFeed()).also { _cache[Feed.Reviews] = it.data }
+        }
+    }
+
+    final override suspend fun fetchNewReleasesFeed(): Result<List<T>, Throwable> {
+        return if (_cache[Feed.NewReleases] != null) {
+            Result.Success(_cache[Feed.NewReleases] as List<T>)
+        } else {
+            delay(300) // simulate network delay
+            return if (simulateFailure.value) Result.Error(failureCause) else Result.Success(newReleasesFeed()).also { _cache[Feed.NewReleases] = it.data }
+        }
+    }
+
+    fun simulateSuccess() {
+        simulateFailure.update { false }
+    }
+
+    fun simulateFailure(cause: Exception? = null) {
+        failureCause = cause
+        simulateFailure.update { true }
+    }
+
+    fun reset() {
+        _cache.putAll(hashMapOf(Feed.News to null, Feed.Reviews to null, Feed.NewReleases to null))
+        simulateFailure.update { false }
+        failureCause = null
+    }
+}
+
+enum class Feed {
+    News,
+    Reviews,
+    NewReleases
+}
--- app/src/test/java/com/mr3y/ludi/core/network/fixtures/TestLogger.kt
+++ app/src/test/java/com/mr3y/ludi/core/network/fixtures/TestLogger.kt
@@ -1,15 +1,42 @@
 package com.mr3y.ludi.core.network.fixtures
 
 import co.touchlab.kermit.ExperimentalKermitApi
+import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
 import co.touchlab.kermit.TestConfig
 import co.touchlab.kermit.TestLogWriter
 
 @OptIn(ExperimentalKermitApi::class)
+fun logger(severity: Severity = Severity.Verbose, logWriter: LogWriter? = null): Logger {
+    return Logger(
+        config = TestConfig(
+            minSeverity = severity,
+            logWriterList = listOf(logWriter ?: TestLogWriter(loggable = severity))
+        )
+    )
+}
+
+@OptIn(ExperimentalKermitApi::class)
 val TestLogger = Logger(
     config = TestConfig(
         minSeverity = Severity.Verbose,
-        logWriterList = listOf(TestLogWriter(loggable = Severity.Verbose))
+        logWriterList = listOf(PrintlnLogWriter())
     ),
 )
+
+class PrintlnLogWriter : LogWriter() {
+
+    private val _logs = mutableListOf<String>()
+    val logs: List<String>
+        get() = _logs
+
+    override fun log(severity: Severity, message: String, tag: String, throwable: Throwable?) {
+        _logs += message
+        if (throwable != null) println("$message $throwable") else println(message)
+    }
+
+    fun reset() {
+        _logs.clear()
+    }
+}
