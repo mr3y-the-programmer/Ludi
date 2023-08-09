9574a07590d5bfe6b704733ee4d9d778f35c8a2b
MR3Y <abdonasr379@gmail.com>
2023-08-09 23:23:08 +0300

    Setup Gradle play publisher & App versioning plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,12 +16,14 @@ plugins {
     alias(libs.plugins.spotless.plugin)
     alias(libs.plugins.firebase.crashlytics)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.gradle.play.publisher)
+    alias(libs.plugins.app.versioning)
 }
 
 android {
     signingConfigs {
         create("release") {
-            if (!System.getenv("CI").toBoolean()) {
+            if (rootProject.file("keystore.properties").exists()) {
                 val keystoreProperties = Properties()
                 keystoreProperties.load(FileInputStream(rootProject.file("keystore.properties")))
                 storeFile = file(keystoreProperties["storeFile"] as String)
@@ -38,8 +40,6 @@ android {
         applicationId = "com.mr3y.ludi"
         minSdk = 26
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.0"
 
         testInstrumentationRunner = "com.mr3y.ludi.runner.CustomTestRunner"
         vectorDrawables {
@@ -98,6 +98,20 @@ android {
             )
         }
     }
+}
+
+play {
+    if(rootProject.file("play_config.json").exists()) {
+        serviceAccountCredentials.set(rootProject.file("play_config.json"))
+        defaultToAppBundles.set(true)
+        track.set("beta")
+    } else {
+        enabled.set(false)
+    }
+}
+
+appVersioning {
+    releaseBuildOnly.set(true)
 }
 
 spotless {
