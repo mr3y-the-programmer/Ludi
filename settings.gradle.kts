fde4571b86dd7970dad011c6a08f571d261bb4c6
M R 3 Y <26522145+mr3y-the-programmer@users.noreply.github.com>
2023-08-22 02:12:49 +0300

    Bump refreshVersions version to 0.60.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("de.fayard.refreshVersions") version "0.51.0"
-////                                # available:"0.60.0"
+        id("de.fayard.refreshVersions") version "0.60.0"
     }
 }
 plugins {
