ca7f6993ecd76c5160c9b9c8375f11c017299872
MR3Y <abdonasr379@gmail.com>
2023-08-29 00:42:10 +0300

    Remove redundant imports

--- app/src/test/java/com/mr3y/ludi/ui/screens/BaseRobolectricTest.kt
+++ app/src/test/java/com/mr3y/ludi/ui/screens/BaseRobolectricTest.kt
@@ -3,8 +3,6 @@ package com.mr3y.ludi.ui.screens
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Before
 import org.junit.Rule
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadows.ShadowLog
 
 open class BaseRobolectricTest {
