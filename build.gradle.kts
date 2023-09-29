ccf2e4ab7e221c2d82b69c71d79b0d190f261014
MR3Y <abdonasr379@gmail.com>
2023-09-29 21:57:58 +0300

    Update plugins { ... } setup in all modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-//    alias(libs.plugins.kotlin.multiplatform) apply false
-//    alias(libs.plugins.kotlin.jvm) apply false
-//    alias(libs.plugins.kotlin.android) apply false
-//    alias(libs.plugins.android.application) apply false
-//    alias(libs.plugins.android.library) apply false
-//    alias(libs.plugins.kotlinSerialization) apply false
-//    alias(libs.plugins.compose.multiplatform) apply false
-    id("org.jetbrains.compose") apply false
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    kotlin("multiplatform").apply(false)
+    id("com.android.application").apply(false)
+    id("com.android.library").apply(false)
+    id("org.jetbrains.compose").apply(false)
 }
