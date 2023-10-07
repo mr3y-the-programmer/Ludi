ddbd5b5a3a113fe96e15a2a8c3e68c8b2ca2d712
MR3Y <abdonasr379@gmail.com>
2023-10-07 23:17:04 +0300

    Move Datastore Serializers implementation to commonMain by leverging OkioStorage

--- shared/src/commonMain/kotlin/com/mr3y/ludi/shared/ui/datastore/FavouriteGamesSerializer.kt
+++ shared/src/commonMain/kotlin/com/mr3y/ludi/shared/ui/datastore/FavouriteGamesSerializer.kt
@@ -1,3 +1,24 @@
 package com.mr3y.ludi.shared.ui.datastore
 
-expect object FavouriteGamesSerializer
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.okio.OkioSerializer
+import com.mr3y.ludi.datastore.model.UserFavouriteGames
+import okio.BufferedSink
+import okio.BufferedSource
+import okio.IOException
+
+object FavouriteGamesSerializer : OkioSerializer<UserFavouriteGames> {
+    override val defaultValue: UserFavouriteGames = UserFavouriteGames()
+
+    override suspend fun readFrom(source: BufferedSource): UserFavouriteGames {
+        try {
+            return UserFavouriteGames.ADAPTER.decode(source)
+        } catch (exception: IOException) {
+            throw CorruptionException("Cannot read UserFavouriteGames proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: UserFavouriteGames, sink: BufferedSink) {
+        UserFavouriteGames.ADAPTER.encode(sink, t)
+    }
+}
