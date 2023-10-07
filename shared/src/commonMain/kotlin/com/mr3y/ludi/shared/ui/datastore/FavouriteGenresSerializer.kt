33b2ae4ecc3ade791600d034d929800325af7821
MR3Y <abdonasr379@gmail.com>
2023-10-07 23:17:04 +0300

    Move Datastore Serializers implementation to commonMain by leverging OkioStorage

--- shared/src/commonMain/kotlin/com/mr3y/ludi/shared/ui/datastore/FavouriteGenresSerializer.kt
+++ shared/src/commonMain/kotlin/com/mr3y/ludi/shared/ui/datastore/FavouriteGenresSerializer.kt
@@ -1,3 +1,24 @@
 package com.mr3y.ludi.shared.ui.datastore
 
-expect object FavouriteGenresSerializer
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.okio.OkioSerializer
+import com.mr3y.ludi.datastore.model.UserFavouriteGenres
+import okio.BufferedSink
+import okio.BufferedSource
+import okio.IOException
+
+object FavouriteGenresSerializer : OkioSerializer<UserFavouriteGenres> {
+    override val defaultValue: UserFavouriteGenres = UserFavouriteGenres()
+
+    override suspend fun readFrom(source: BufferedSource): UserFavouriteGenres {
+        try {
+            return UserFavouriteGenres.ADAPTER.decode(source)
+        } catch (exception: IOException) {
+            throw CorruptionException("Cannot read UserFavouriteGenres proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: UserFavouriteGenres, sink: BufferedSink) {
+        UserFavouriteGenres.ADAPTER.encode(sink, t)
+    }
+}
--- shared/src/commonMain/kotlin/com/mr3y/ludi/shared/ui/datastore/FollowedNewsDataSourceSerializer.kt
+++ shared/src/commonMain/kotlin/com/mr3y/ludi/shared/ui/datastore/FollowedNewsDataSourceSerializer.kt
@@ -1,3 +1,24 @@
 package com.mr3y.ludi.shared.ui.datastore
 
-expect object FollowedNewsDataSourceSerializer
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.okio.OkioSerializer
+import com.mr3y.ludi.datastore.model.FollowedNewsDataSources
+import okio.BufferedSink
+import okio.BufferedSource
+import okio.IOException
+
+object FollowedNewsDataSourceSerializer : OkioSerializer<FollowedNewsDataSources> {
+    override val defaultValue: FollowedNewsDataSources = FollowedNewsDataSources()
+
+    override suspend fun readFrom(source: BufferedSource): FollowedNewsDataSources {
+        try {
+            return FollowedNewsDataSources.ADAPTER.decode(source)
+        } catch (exception: IOException) {
+            throw CorruptionException("Cannot read FollowedNewsDataSources proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: FollowedNewsDataSources, sink: BufferedSink) {
+        FollowedNewsDataSources.ADAPTER.encode(sink, t)
+    }
+}
