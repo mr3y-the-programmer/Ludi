980406d9f02169d04bd89ffb14423552935a7c24
MR3Y <abdonasr379@gmail.com>
2023-10-09 22:01:30 +0300

    Fix Deal content description formatting errors

--- shared/src/desktopAndroidMain/kotlin/com/mr3y/ludi/shared/ui/resources/format.shared.kt
+++ shared/src/desktopAndroidMain/kotlin/com/mr3y/ludi/shared/ui/resources/format.shared.kt
@@ -1,3 +1,7 @@
 package com.mr3y.ludi.shared.ui.resources
 
+import java.math.RoundingMode
+
 actual fun String.frmt(vararg args: Any?) = format(args)
+
+actual fun Float.roundToTwoDecimalDigits(): Float = toBigDecimal().setScale(2, RoundingMode.HALF_UP).toFloat()
