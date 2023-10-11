baedc39f4da0b142e932dca69c77de64a9020998
MR3Y <abdonasr379@gmail.com>
2023-10-11 21:01:45 +0300

    Setup Android App

--- androidApp/src/main/kotlin/com/mr3y/ludi/core/CrashReporting.kt
+++
@@ -1,6 +0,0 @@
-package com.mr3y.ludi.core
-
-interface CrashReporting {
-
-    fun recordException(throwable: Throwable, logMessage: String?)
-}
--- androidApp/src/main/kotlin/com/mr3y/ludi/di/annotations/Singleton.kt
+++
@@ -1,7 +0,0 @@
-package com.mr3y.ludi.di.annotations
-
-import me.tatarka.inject.annotations.Scope
-
-@Scope
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER)
-annotation class Singleton
