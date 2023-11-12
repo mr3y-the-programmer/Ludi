ffa15b170c731628d9c00db22487b88d3769ed4a
MR3Y <abdonasr379@gmail.com>
2023-11-13 00:03:25 +0200

    Migrate from systemuicontroller to enableEdgeToEdge

--- androidApp/src/main/kotlin/com/mr3y/ludi/MainActivity.kt
+++ androidApp/src/main/kotlin/com/mr3y/ludi/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.mr3y.ludi
 
 import android.app.Activity
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.mr3y.ludi.shared.App
 import com.mr3y.ludi.shared.di.AndroidApplicationComponent
 import com.mr3y.ludi.shared.di.HostActivityComponent
@@ -35,25 +36,21 @@ class MainActivity : ComponentActivity(), HostActivityComponentOwner {
         // Initialize the component.
         hostActivityComponent
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         val appState = hostActivityComponent.parent.appState
         lifecycleScope.launch {
             appState.preferences.flowWithLifecycle(lifecycle, Lifecycle.State.CREATED).collect { userPreferences ->
                 splashScreen.setKeepOnScreenCondition { userPreferences == null }
             }
         }
+
+        goEdgeToEdge()
+
         setContent {
             val userPreferences by appState.preferences.collectAsStateWithLifecycle(minActiveState = Lifecycle.State.CREATED)
-            val systemUiController = rememberSystemUiController()
             val useDarkIcons = !isSystemInDarkTheme()
 
-            DisposableEffect(systemUiController, useDarkIcons) {
-                // Update all of the system bar colors to be transparent, and use
-                // dark icons if we're in light theme
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = useDarkIcons
-                )
+            DisposableEffect(useDarkIcons) {
+                goEdgeToEdge(isDarkTheme = { !useDarkIcons })
 
                 onDispose {}
             }
@@ -61,16 +58,16 @@ class MainActivity : ComponentActivity(), HostActivityComponentOwner {
                 App(
                     isDarkTheme = when (userPreferences!!.theme) {
                         Theme.Light -> {
-                            systemUiController.setSystemBarsColor(Color.Transparent, true)
+                            goEdgeToEdge(isDarkTheme = { false })
                             false
                         }
                         Theme.Dark -> {
-                            systemUiController.setSystemBarsColor(Color.Transparent, false)
+                            goEdgeToEdge(isDarkTheme = { true })
                             true
                         }
                         Theme.SystemDefault -> {
                             val isDarkTheme = isSystemInDarkTheme()
-                            systemUiController.setSystemBarsColor(Color.Transparent, !isDarkTheme)
+                            goEdgeToEdge(isDarkTheme = { isDarkTheme })
                             isDarkTheme
                         }
                     },
@@ -79,6 +76,25 @@ class MainActivity : ComponentActivity(), HostActivityComponentOwner {
                 )
             }
         }
+    }
+
+    private fun goEdgeToEdge(
+        isDarkTheme: (Resources) -> Boolean = { resources ->
+            (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+        }
+    ) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT,
+                detectDarkMode = isDarkTheme
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF),
+                darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b),
+                detectDarkMode = isDarkTheme
+            )
+        )
     }
 }
 
