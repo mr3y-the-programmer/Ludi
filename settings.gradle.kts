54619171592370fe3863f01ecfaf58eb321fdc34
MR3Y <abdonasr379@gmail.com>
2024-02-06 01:42:53 +0200

    Bump refreshVersions to 0.60.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     plugins {
-        id("de.fayard.refreshVersions") version "0.60.3"
+        id("de.fayard.refreshVersions") version "0.60.5"
     }
 }
 buildscript {
