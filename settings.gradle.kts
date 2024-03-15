bb4001678a062e3f86d48d0cd3b463b9964d5cc7
MR3Y <abdonasr379@gmail.com>
2024-03-15 22:13:44 +0200

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 buildscript {
     // Workaround for: https://github.com/Splitties/refreshVersions/issues/707
     dependencies {
-        classpath("com.squareup.okio:okio:3.8.0")
+        classpath("com.squareup.okio:okio:3.9.0")
     }
 }
 plugins {
